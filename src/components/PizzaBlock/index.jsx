1175a00cf07ea99088d4dafa512249a3e3f82eec
ElzaEldarh <bbheldarhanova2201@mail.ru>
2025-04-26 15:08:05 +0300

    добавила адаптив на карточки пицц

--- src/components/PizzaBlock/index.jsx
+++ src/components/PizzaBlock/index.jsx
@@ -6,7 +6,8 @@ const PizzaBlock = ({ title, price, imageUrl, sizes, types }) => {
   const typeNames = ["тонкое", "традиционное"];
 
   return (
-    <div className="pizza-block">
+    <div className="pizza-block-wrapper">
+      <div className="pizza-block">
       <img className="pizza-block__image" src={imageUrl} alt="Pizza" />
       <h4 className="pizza-block__title">{title}</h4>
       <div className="pizza-block__selector">
@@ -54,6 +55,7 @@ const PizzaBlock = ({ title, price, imageUrl, sizes, types }) => {
           <i>2</i>
         </button>
       </div>
+    </div>
     </div>
   );
 };
