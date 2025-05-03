2742899377a14edc379d460624d84c4e95dc11d3
ElzaEldarh <bbheldarhanova2201@mail.ru>
2025-05-03 18:02:46 +0300

    добавила элемент поиска и пагинацию

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,20 @@ import Header from "./components/header";
 import Home from "./pages/home";
 import Cart from "./pages/cart"
 import NotFound from "./pages/notFound";
-import "./scss/app.scss";
 import { Route, Routes } from "react-router-dom";
+import { useState } from "react";
+import "./scss/app.scss";
 
 const App = () => {
+  const[searchValue, setSearchValue]=useState('')
   return (
     <>
       <div className="wrapper">
-        <Header />
+        <Header searchValue={searchValue} setSearchValue={setSearchValue} />
         <div className="content">
   
           <Routes>
-            <Route path="/" element={<Home/>}></Route>
+            <Route path="/" element={<Home searchValue={searchValue} />}></Route>
             <Route path="/cart" element={<Cart/>}></Route>
             <Route path="*" element={<NotFound/>}></Route>
           </Routes>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,10 +2,13 @@ import Categories from "../components/categories";
 import Sort from "../components/sort";
 import PizzaBlock from "../components/PizzaBlock/index";
 import Skeleton from "../components/PizzaBlock/skeleton";
-import { useEffect, useState } from "react";
 
-const Home = () => {
+import { useEffect, useState } from "react";
+import Pagination from "../components/Pagination";
+
+const Home = ({ searchValue }) => {
   const [items, setItems] = useState([]);
+  const [currentPage, setCurrentPage]=useState(1)
   const [isLoading, setIsLoading] = useState(true);
   const [categoryId, setCategoryId] = useState(0);
   const [sortType, setSortType] = useState({
@@ -15,19 +18,37 @@ const Home = () => {
 
   useEffect(() => {
     setIsLoading(true);
+    const search = searchValue ? `&search=${searchValue}` : "";
+
     fetch(
-      `https://67fd003b3da09811b1744c6c.mockapi.io/pizzas?${
-        categoryId > 0 ? `category=${categoryId}` : ""
-      }&sortBy=${sortType.sortProperty}&order=desc`
+      `https://67fd003b3da09811b1744c6c.mockapi.io/pizzas?page=${currentPage}&limit=4${
+        categoryId > 0 ? `&category=${categoryId}` : ""
+      }&sortBy=${sortType.sortProperty}&order=desc${search}`
     )
+    
       .then((res) => res.json())
-      .then((arr) => setItems(arr))
+      .then((arr) => {
+        setItems(arr);
+        setIsLoading(false);
+      })
       .catch((err) => {
         console.warn("Ошибка при получении пицц:", err);
+        setIsLoading(false);
       });
-    setIsLoading(false);
+
     window.scrollTo(0, 0);
-  }, [categoryId, sortType]);
+  }, [categoryId, sortType, searchValue, currentPage]);
+
+  const pizzas = Array.isArray(items)
+  ? items
+      .filter((obj) =>
+        obj.title.toLowerCase().includes(searchValue.toLowerCase())
+      )
+      .map((obj) => <PizzaBlock key={obj.id} {...obj} />)
+  : [];
+  const skeletons = [...new Array(6)].map((_, index) => (
+    <Skeleton key={index} />
+  ));
 
   return (
     <div className="container">
@@ -39,11 +60,8 @@ const Home = () => {
         <Sort sortType={sortType} onChangeSort={(i) => setSortType(i)} />
       </div>
       <h2 className="content__title">Все пиццы</h2>
-      <div className="content__items">
-        {isLoading
-          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-          : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}{" "}
-      </div>
+      <div className="content__items">{isLoading ? skeletons : pizzas} </div>
+      <Pagination onChangePage={number=> setCurrentPage(number)} />
     </div>
   );
 };
